9a9f5f662546ad37241bf6fb1a2f7a9723a8023b
Thijs Kamphuis <thijskamphuis@icloud.com>
2023-01-18 22:44:47 +0100

    a

--- autorun.py
+++ autorun.py
@@ -1,7 +1,8 @@
 import subprocess
 
+subprocess.Popen(["git", "fetch"])
 p = subprocess.Popen(["git", "status", "-uno"], stdout=subprocess.PIPE)
 
-s = p.stdout.readline().decode()
-#
+s = [l.decode() for l in p.stdout.readlines()]
+
 print(s)
