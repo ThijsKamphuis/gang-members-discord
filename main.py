5287448d31cc18f2ef046ae2a862d0f7a6deade7
Thijs Kamphuis <thijskamphuis@icloud.com>
2023-03-01 23:47:40 +0100

    added events

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from turtle import width
 import discord
 from discord.ext import commands
 from discord.ext import tasks
@@ -14,22 +15,12 @@ from re import sub
 from num2words import num2words
 import mysql.connector
 
-
 #### SETUP ###################################################
 load_dotenv()
 
 #### SQL ####
-event_db = mysql.connector.connect(
-    host= os.getenv('SQL_HOST'),
-    user= os.getenv('SQL_USER'),
-    password= os.getenv('SQL_PASS'),
-    database= os.getenv('SQL_DB')
-)
 
-event_db_cursor = event_db.cursor()
 
-#sql = "INSERT INTO events (id, owner, title, content, creationdate, creationtime, eventdate, eventtime, maxvisitors, photo, location, visibility) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-#val = (random.randint(10000000000,99999999999), "65872", "TestGM", "pp", "28-02-2023", "22:10", "28-02-2023", "23:23", "69", "", "", "visible")
 
 #### BOT INIT ####
 class GangMemberBot(discord.Bot):
@@ -331,13 +322,11 @@ async def motmstandings_role_error(ctx: discord.ApplicationContext, error: disco
     
 #### RESET VOTES ####
 def reset_voting():
-    # Generate archive name (prev month)
-    archive_file_name = f"votes_{(datetime.now().month) - 1}_{datetime.now().year}"
+
+    archive_file_name = f"votes_{datetime.now().month}_{datetime.now().year}"
     
-    # Copy votes to archive
     open(f"archive/{archive_file_name}.json", "w").write(open("databases/motm_votes.json").read())
     
-    # Empty votes
     with open('databases/motm_votes.json', 'w') as outfile:
         json.dump([], outfile, indent=4)
         
@@ -386,8 +375,8 @@ async def check_for_month():
 
     motm_month = (date.today().month)
     first_of_month = datetime(date.today().year, motm_month, 1, hour=0, minute=1)
-        
-    if (first_of_month <= datetime.now() <= (datetime.now() + timedelta(minutes=1))):
+    
+    if (first_of_month <= datetime.now() <= (first_of_month + timedelta(minutes=1))):
         count_votes()
         if vote_standings[0][1] == vote_standings[1][1]:
             if random.randint(1,2) == 2:
@@ -543,6 +532,130 @@ async def sendmsg_error(ctx: discord.ApplicationContext, error: discord.DiscordE
 
 
 #### EVENTS ###################################################
+#### SEND EVENT BY ID ####
+@bot.slash_command(label="sendevent", description="share an event from noterix.com")
+@commands.has_any_role(GMStaff_id, GMAdmin_id)
+async def sendevent(ctx: discord.ApplicationContext, eventid: str):
+    
+    event_db = mysql.connector.connect(
+    host= os.getenv('SQL_HOST'),
+    user= os.getenv('SQL_USER'),
+    password= os.getenv('SQL_PASS'),
+    database= os.getenv('SQL_DB')
+    )
+    event_db_cursor = event_db.cursor()
+    
+    event_db_cursor.execute(f"SELECT * FROM events WHERE id = '{eventid}'")
+    event_details = (event_db_cursor.fetchall())[0]
+    event_db.close()
+
+    event_embed = discord.Embed(
+        title = event_details[2],
+        description = event_details[3].replace("<br />", "\n"),
+        url=f"https://noterix.com/event?id={event_details[0]}",
+        color=int("4540619")
+    )
+    event_embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/914862282335453215/1067193702038110228/favicon.png")
+    event_embed.set_image(url=event_details[9])
+    event_embed.set_footer(text=f"https://noterix.com/event?id={event_details[0]}\nEvent hosted with Noterix.com")
+    
+    event_embed.add_field(
+        name="Date",
+        value=event_details[6],
+        inline=True
+    )
+    event_embed.add_field(
+        name="Time",
+        value=event_details[7],
+        inline=True
+    )
+    event_embed.add_field(
+        name="Max visitors",
+        value=event_details[8],
+        inline=True
+    )
+    event_embed.add_field(
+        name="Location",
+        value=event_details[10],
+        inline=False
+    )  
+    
+    
+    await ctx.respond(embed= event_embed)
+
+        
+@sendevent.error
+async def sendevent_error(ctx: discord.ApplicationContext, error: discord.DiscordException):
+    if isinstance(error, commands.MissingAnyRole):
+        await ctx.respond("You do not have permission to use this command. (STAFF ONLY)", ephemeral=True)
+    else:
+        raise error
+
+#### ADD EVENT ####
+@bot.slash_command(label="addevent", description="share an event from noterix.com")
+@commands.has_any_role(GMStaff_id, GMAdmin_id)
+async def addevent(ctx: discord.ApplicationContext, title: str, content: str, eventdate: str, eventtime: str, maxvisitors: int, location: str):
+    event_db = mysql.connector.connect(
+    host= os.getenv('SQL_HOST'),
+    user= os.getenv('SQL_USER'),
+    password= os.getenv('SQL_PASS'),
+    database= os.getenv('SQL_DB')
+    )
+    event_db_cursor = event_db.cursor()
+    
+    event_id = random.randint(10000000000,99999999999)
+    sql = "INSERT INTO events (id, owner, title, content, creationdate, creationtime, eventdate, eventtime, maxvisitors, photo, location, visibility) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    val = (event_id, "65872", title, content, date.today().strftime("%d-%m-%Y"), datetime.now().strftime("%H:%M"), eventdate, eventtime, maxvisitors, "", location, "visible")
+    event_db_cursor.execute(sql, val)
+    event_db.commit()
+    event_db.close()
+
+
+
+    event_embed = discord.Embed(
+        title = title,
+        description = content,
+        url=f"https://noterix.com/event?id={event_id}",
+        color=int("4540619")
+    )
+    event_embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/914862282335453215/1067193702038110228/favicon.png")
+    event_embed.set_footer(text=f"https://noterix.com/event?id={event_id}\nEvent hosted with Noterix.com")
+    
+    event_embed.add_field(
+        name="Date",
+        value=eventdate,
+        inline=True
+    )
+    event_embed.add_field(
+        name="Time",
+        value=eventtime,
+        inline=True
+    )
+    event_embed.add_field(
+        name="Max visitors",
+        value=maxvisitors,
+        inline=True
+    )
+    event_embed.add_field(
+        name="Location",
+        value=location,
+        inline=False
+    )  
+    
+    
+    await ctx.respond(embed= event_embed)
+
+        
+@addevent.error
+async def addevent_error(ctx: discord.ApplicationContext, error: discord.DiscordException):
+    if isinstance(error, commands.MissingAnyRole):
+        await ctx.respond("You do not have permission to use this command. (STAFF ONLY)", ephemeral=True)
+    else:
+        raise error
+
+
+
+
 
 
 bot.run(os.getenv('DISCORD_TOKEN'))
