d9ef05be5193038b3cbdf92e0e6b2380f6a07fdb
Thijs Kamphuis <thijskamphuis@icloud.com>
2023-03-09 23:10:04 +0100

    added pfp commands

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from unicodedata import name
 import discord
 from discord.ext import commands
 import discord.utils
 import os
 from dotenv import load_dotenv
 from num2words import num2words
+import requests
+
 
 #### SETUP ####
 load_dotenv()
@@ -53,4 +56,19 @@ async def on_member_join(member):
     count_suffix = num2words(member_count, to='ordinal')[-2:]
     await bot.get_channel(882248303822123021).send(f"Hello <@{member.id}>, welcome to Gang Members. You are the {member_count}{count_suffix} member to join.")
 
+
+@bot.slash_command(name="downloadpfps", description="download pfp of every member (STAFF ONLY)")
+@commands.has_any_role(GMStaff_id, GMAdmin_id)
+async def download_pfps(ctx):
+    for member in (bot.get_guild(gm_guild_id).members):
+        print(member)
+        open(f"profilepics/{member.name}.png", "wb").write(requests.get(member.display_avatar.url, allow_redirects=True).content)
+                
+@download_pfps.error
+async def motmvote_role_error(ctx: discord.ApplicationContext, error: discord.DiscordException):
+    if isinstance(error, commands.MissingAnyRole):
+        await ctx.respond("You do not have permission to use this command. (STAFF ONLY)", ephemeral=True)
+    else:
+        raise error
+    
 bot.run(os.getenv('DISCORD_TOKEN'))
