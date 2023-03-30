f1ba823ea2b32a29eb47ff8ac64c2fbcc7f97159
Thijs Kamphuis <thijskamphuis@icloud.com>
2023-03-30 02:04:09 +0200

    birthday

--- cogs/birthday.py
+++ cogs/birthday.py
@@ -4,6 +4,9 @@ import discord.utils
 import json
 from datetime import datetime
 
+
+gm_guild_id = 882248303822123018
+
 class birthday(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -22,11 +25,11 @@ class birthday(commands.Cog):
             
         with open('databases/birthdays.json', 'w') as outfile:
                 json.dump(birthdaysdb, outfile, indent=4)
-        return
                 
-      
-    @commands.slash_command(name="birthdaylist", description="View upcoming birthdays.")
-    async def birthdayset(self, ctx: discord.ApplicationContext):
+                
+
+    @commands.slash_command(name="nextbirthday", description="View upcoming birthdays.")
+    async def nextbirthday(self, ctx: discord.ApplicationContext):
         og_birthdaysdb = json.load(open('databases/birthdays.json', encoding="utf-8"))
 
         birthdays = {k: v[:-4] + str(datetime.now().year) for k, v in og_birthdaysdb.items()}
@@ -38,22 +41,22 @@ class birthday(commands.Cog):
                 birthdays[k] += 365
 
 
-        birthdays = dict(sorted(birthdays.items(), key=lambda x: x[1]))
-        birthdays = {k: og_birthdaysdb[k] for k, v in birthdays.items()}
+        days_birthdays = dict(sorted(birthdays.items(), key=lambda x: x[1]))
+        sorted_birthdays = {k: og_birthdaysdb[k] for k, v in days_birthdays.items()}
         
         birthdays_embed = discord.Embed(
-            title= "Gang Members Birthdays",
+            title= "Upcoming birthdays",
             color=0xae8cff
         )
         birthdays_embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/914862282335453215/1067193702038110228/favicon.png")
-
-        for birthday in birthdays:
+        
+        for birthday in sorted_birthdays:
             birthdays_embed.add_field(
-                name= birthday,
-                value=birthdays[birthday],
+                name= f"{self.bot.get_guild(gm_guild_id).get_member(int(birthday)).name} {f'({self.bot.get_guild(gm_guild_id).get_member(int(birthday)).nick})' if self.bot.get_guild(gm_guild_id).get_member(int(birthday)).nick else ''}",
+                value=f"""Turns {datetime.now().year - datetime.strptime(sorted_birthdays[birthday][-4:], '%Y').year if datetime.strptime(sorted_birthdays[birthday], '%d-%m-%Y').replace(year=datetime.today().year) >= datetime.today() else ((datetime.now().year - datetime.strptime(sorted_birthdays[birthday][-4:], '%Y').year) + 1)} in {days_birthdays[birthday]} days, {datetime.strptime(sorted_birthdays[birthday], '%d-%m-%Y').strftime('%d-%b-%Y')}""",
                 inline=False
             )
-            
+          
         await ctx.respond(embed=birthdays_embed, ephemeral=True)
         
         
